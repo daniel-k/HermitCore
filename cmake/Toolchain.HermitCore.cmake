32f71637558923f60ccbaf1772c975c938e8afb3
Daniel Krebs <github@daniel-krebs.net>
2017-01-29 14:52:26 +0100

    cmake: add support for Go and also enable Fortran

--- cmake/Toolchain.HermitCore.cmake
+++ cmake/Toolchain.HermitCore.cmake
@@ -3,8 +3,10 @@ set(TARGET x86_64-hermit)
 
 set(CMAKE_SYSTEM_NAME Generic)
 
-# CMake will also use the path to search for binutils and C++ compiler
-set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET}-gcc)
 
-# apparently cmake will also look for libraries there
-#set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_DIR}/${TARGET})
+# point CMake to our toolchain, it will auto detect binutils and C++ compiler
+set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET}-gcc)
+set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET}-gfortran)
+set(CMAKE_Go_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET}-gccgo)
+
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/golang/)
