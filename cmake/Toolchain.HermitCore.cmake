bdc31ae33a71d49be789dafa289b8f2dc8df09ba
Daniel Krebs <github@daniel-krebs.net>
2017-01-29 14:52:27 +0100

    cmake/Toolchain: hint CMake about toolchain prefix to find binutils

--- cmake/Toolchain.HermitCore.cmake
+++ cmake/Toolchain.HermitCore.cmake
@@ -1,9 +1,12 @@
 set(TOOLCHAIN_DIR /opt/hermit)
-set(TARGET x86_64-hermit)
+set(TARGET_ARCH x86_64-hermit)
 
 set(CMAKE_SYSTEM_NAME Generic)
 
-# point CMake to our toolchain, it will auto detect binutils and C++ compiler
-set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET}-gcc)
-set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET}-gfortran)
-set(CMAKE_Go_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET}-gccgo)
+# point CMake to our toolchain, it will auto detect C++ compiler
+set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET_ARCH}-gcc)
+set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET_ARCH}-gfortran)
+set(CMAKE_Go_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET_ARCH}-gccgo)
+
+# hinting the prefix is needed in order to correctly detect binutils
+set(_CMAKE_TOOLCHAIN_PREFIX "${TARGET_ARCH}-")
