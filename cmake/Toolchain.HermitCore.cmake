4654be3734dae48f5175d056c1983cd1faef7703
Daniel Krebs <github@daniel-krebs.net>
2017-01-29 14:52:27 +0100

    cmake: also hint location for toolchain

--- cmake/Toolchain.HermitCore.cmake
+++ cmake/Toolchain.HermitCore.cmake
@@ -1,12 +1,14 @@
-set(TOOLCHAIN_DIR /opt/hermit)
+set(TOOLCHAIN_BIN_DIR /opt/hermit/bin)
 set(TARGET_ARCH x86_64-hermit)
 
 set(CMAKE_SYSTEM_NAME Generic)
 
 # point CMake to our toolchain, it will auto detect C++ compiler
-set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET_ARCH}-gcc)
-set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET_ARCH}-gfortran)
-set(CMAKE_Go_COMPILER ${TOOLCHAIN_DIR}/bin/${TARGET_ARCH}-gccgo)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_ARCH}-gcc)
+set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_ARCH}-gfortran)
+set(CMAKE_Go_COMPILER ${TOOLCHAIN_BIN_DIR}/${TARGET_ARCH}-gccgo)
 
-# hinting the prefix is needed in order to correctly detect binutils
+# hinting the prefix and location is needed in order to correctly detect
+# binutils
 set(_CMAKE_TOOLCHAIN_PREFIX "${TARGET_ARCH}-")
+set(_CMAKE_TOOLCHAIN_LOCATION ${TOOLCHAIN_BIN_DIR})
