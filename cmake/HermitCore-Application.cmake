10f572c24ae43d39be6bffbcf85c37296be146ef
Daniel Krebs <github@daniel-krebs.net>
2017-03-16 17:09:29 +0100

    cmake: add new include file for applications (tests, benchmarks, openmpbench)

---
+++ cmake/HermitCore-Application.cmake
@@ -0,0 +1,8 @@
+include(${CMAKE_CURRENT_LIST_DIR}/HermitCore.cmake)
+
+add_compile_options(${HERMIT_APP_FLAGS})
+
+# link against local libhermit.a instead of the one supplied with the
+# toolchain, if built from top-level
+link_directories(${EXTERNALS_LIB_DIR})
+
