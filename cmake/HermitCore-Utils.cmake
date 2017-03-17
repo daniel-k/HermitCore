0a7649baf6c0867e2e565d31a1d82665ffb956e8
Daniel Krebs <github@daniel-krebs.net>
2017-03-17 16:03:41 +0100

    cmake: get rid off deploy() macro

--- cmake/HermitCore-Utils.cmake
+++ cmake/HermitCore-Utils.cmake
@@ -14,13 +14,16 @@ macro(add_kernel_module_sources MODULE SOURCE_GLOB)
     list(APPEND "_KERNEL_SOURCES_${MODULE}" "${SOURCES}")
 endmacro(add_kernel_module_sources)
 
+
 macro(get_kernel_module_sources VAR MODULE)
     set(${VAR} ${_KERNEL_SOURCES_${MODULE}})
 endmacro(get_kernel_module_sources)
 
+
 macro(get_kernel_modules VAR)
 	set(${VAR} ${_KERNEL_MODULES})
 endmacro(get_kernel_modules)
+
 
 # find program in /toolchain/dir/prefix-NAME, only supply NAME
 macro(find_toolchain_program NAME)
@@ -40,38 +43,6 @@ macro(find_toolchain_program NAME)
     endif()
 endmacro(find_toolchain_program)
 
-macro(deploy TARGETS DESTINATION COMPONENT)
-	install(TARGETS ${TARGETS}
-		DESTINATION ${DESTINATION}
-		COMPONENT   ${COMPONENT})
-
-	# Create hierarchical uninstall targets: uninstall[-COMPONENT[-TARGET]]
-
-	if(NOT TARGET uninstall)
-		add_custom_target(uninstall)
-	endif()
-
-	set(COMPONENT_UNINSTALL_TARGET uninstall-${COMPONENT})
-	if(NOT TARGET ${COMPONENT_UNINSTALL_TARGET})
-		add_custom_target(${COMPONENT_UNINSTALL_TARGET})
-		add_dependencies(uninstall ${COMPONENT_UNINSTALL_TARGET})
-		# message("add component uninstall target: ${COMPONENT_UNINSTALL_TARGET}")
-	endif()
-
-	foreach(TARGET ${TARGETS})
-		set(TARGET_UNINSTALL_TARGET ${COMPONENT_UNINSTALL_TARGET}-${TARGET})
-		# message("add target uninstall target: ${TARGET_UNINSTALL_TARGET}")
-
-		add_custom_target(${TARGET_UNINSTALL_TARGET}
-			COMMAND
-				${CMAKE_COMMAND} -E remove_directory ${DESTINATION}/$<TARGET_FILE:${TARGET}>
-			COMMAND
-				rmdir --ignore-fail-on-non-empty ${DESTINATION})
-
-		add_dependencies(${COMPONENT_UNINSTALL_TARGET} ${TARGET_UNINSTALL_TARGET})
-
-	endforeach()
-endmacro(deploy)
 
 macro(set_parent VAR VALUE)
 	set(${VAR} ${VALUE} PARENT_SCOPE)
