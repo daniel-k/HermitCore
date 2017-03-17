bbc4010af886fc64dd56e813819a7ddde9919e92
Daniel Krebs <github@daniel-krebs.net>
2017-03-17 12:00:05 +0100

    cmake: fix linking against freshly built libraries
    Until now, linker defaulted to toolchain supplied libraries.

--- cmake/HermitCore-Application.cmake
+++ cmake/HermitCore-Application.cmake
@@ -4,5 +4,5 @@ add_compile_options(${HERMIT_APP_FLAGS})
 
 # link against local libhermit.a instead of the one supplied with the
 # toolchain, if built from top-level
-link_directories(${EXTERNALS_LIB_DIR})
+link_directories(${EXTERNALS_LIB_ARCH_DIR})
 
