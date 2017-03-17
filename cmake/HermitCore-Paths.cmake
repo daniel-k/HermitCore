8ba7cc50352dbd59b923dc8d18d36d9dde6db3d7
Daniel Krebs <github@daniel-krebs.net>
2017-03-17 12:23:16 +0100

    cmake: rename EXTERNALS_* to LOCAL_PREFIX_*

--- cmake/HermitCore-Paths.cmake
+++ cmake/HermitCore-Paths.cmake
@@ -4,16 +4,23 @@ set(HERMIT_ROOT ${CMAKE_CURRENT_LIST_DIR}/..)
 # where to install compiled binaries
 set(HERMIT_BIN_DIR ${HERMIT_ROOT}/bin)
 
-# we define our own install prefix
+# define our own install prefix
 set(CMAKE_INSTALL_PREFIX /opt/hermit)
 
-# we install 3rd party libraries to an intermediate directory and relocate them
-# later when installing the whole project
-if(NOT EXTERNALS_BASE_DIR)
+# we install 3rd party libraries to an intermediate directory and relocate
+# them here later when installing the whole project
+if(NOT LOCAL_PREFIX_BASE_DIR)
 	# will be injected into external project because CMAKE_BINARY_DIR will be
 	# different there
-	set(EXTERNALS_BASE_DIR ${CMAKE_BINARY_DIR}/externals)
+	set(LOCAL_PREFIX_BASE_DIR ${CMAKE_BINARY_DIR}/local_prefix)
 endif()
-set(EXTERNALS_PREFIX_DIR ${EXTERNALS_BASE_DIR}/${CMAKE_INSTALL_PREFIX})
-set(EXTERNALS_LIB_DIR ${EXTERNALS_PREFIX_DIR}/lib)
-set(EXTERNALS_LIB_ARCH_DIR ${EXTERNALS_PREFIX_DIR}/${TARGET_ARCH}/lib)
+
+# during build process libraries and external projects will be deployed into
+# this directory structure
+set(LOCAL_PREFIX_DIR ${LOCAL_PREFIX_BASE_DIR}/${CMAKE_INSTALL_PREFIX})
+set(LOCAL_PREFIX_ARCH_DIR ${LOCAL_PREFIX_DIR}/${TARGET_ARCH})
+
+# when building applications within the HermitCore project (tests, ...) they
+# will link prefarably against libraries in this directory in order to test
+# changes in the kernel
+set(LOCAL_PREFIX_ARCH_LIB_DIR ${LOCAL_PREFIX_ARCH_DIR}/lib)
